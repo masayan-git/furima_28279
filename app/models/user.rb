ae446a4c9d0ba307250e006e7876fb09009047b7
masayan-git <68589347+masayan-git@users.noreply.github.com>
2020-08-14 14:31:03 +0900

    修正１

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,17 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   with_options presence: true do
-    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-    validates :first_name_reading, format: { with: /\A[ァ-ヶー－]+\z/ }
-    validates :last_name_reading, format: { with: /\A[ァ-ヶー－]+\z/ }
-    validates :email, format: { with: /\A\S+@\S+\.\S+\z/ }
-    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
-    validates :nickname
+    zenkaku_ja = /\A[ぁ-んァ-ン一-龥]/
+    katakana = /\A[ァ-ヶー－]+\z/
+    email = /\A\S+@\S+\.\S+\z/
+    password = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i
+    validates :first_name, format: { with: zenkaku_ja }
+    validates :last_name, format: { with: zenkaku_ja }
+    validates :first_name_reading, format: { with: katakana }
+    validates :last_name_reading, format: { with: katakana }
+    validates :email, format: { with: email }
+    validates :password, format: { with: password }
+    validates :nickname, uniqueness: { case_sensitive: true }
     validates :birthday
   end
 end
