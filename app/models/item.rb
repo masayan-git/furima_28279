2a03b04820236239f70ba4290588a7c3ef3c91d6
masayan-git <68589347+masayan-git@users.noreply.github.com>
2020-08-16 16:55:38 +0900

    修正１

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,13 +7,14 @@ class Item < ApplicationRecord
     validates :name, length: { maximum: 40, too_long: 'minimum is %{count}characters' }
     validates :overview, length: { maximum: 1000, too_long: 'minimum is %{count}characters' }
   end
-
+  with_options numericality: { other_than: 0, message: "can't be blank" } do
+    validates :area
+    validates :category
+    validates :delivery_fee
+    validates :shipping
+    validates :status
+  end
   validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'is out of setting range' }
-  validates :area, numericality: { other_than: 0, message: "can't be blank" }
-  validates :category, numericality: { other_than: 0, message: "can't be blank" }
-  validates :delivery_fee, numericality: { other_than: 0, message: "can't be blank" }
-  validates :shipping, numericality: { other_than: 0, message: "can't be blank" }
-  validates :status, numericality: { other_than: 0, message: "can't be blank" }
 
   def image_presence
     if image.attached?
