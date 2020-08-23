12183e5a5ec3dd724db8f48b067699e4a87bc09a
masayan-git <68589347+masayan-git@users.noreply.github.com>
2020-08-23 18:46:14 +0900

    仮　削除

--- app/models/orderer.rb
+++ app/models/orderer.rb
@@ -1,8 +1,3 @@
 class Orderer < ApplicationRecord
-  extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :area
-  belongs_to_active_hash :category
-  belongs_to_active_hash :delivery_fee
-  belongs_to_active_hash :shipping
-  belongs_to_active_hash :status
+  # 使用していません
 end
