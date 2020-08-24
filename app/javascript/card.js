cb9c94ddcb95f07af805d5c5b6be93c5581580f3
masayan-git <68589347+masayan-git@users.noreply.github.com>
2020-08-24 12:42:26 +0900

    デバッグ用の記述削除

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -6,8 +6,6 @@ const pay = () => {
 
     const formResult = document.getElementById("charge-form");
     const formData = new FormData(formResult);
-    console.log(formResult)
-    console.log(formData)
     const card = {
       number: formData.get("procedure_address[number]"),
       cvc: formData.get("procedure_address[cvc]"),
@@ -16,8 +14,6 @@ const pay = () => {
     };
     
     Payjp.createToken(card, (status, response) => {
-      console.log(response)
-      console.log(status)
       if (status === 200) {
         const token = response.id;
         const renderDom = document.getElementById("charge-form");
